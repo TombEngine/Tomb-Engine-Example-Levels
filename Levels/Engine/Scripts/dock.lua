c4d96fb728d70a7a3e9f809200d22bec996fc724
Stranger1992 <84292688+Stranger1992@users.noreply.github.com>
2022-08-08 18:11:06 +0100

    ClawTests 1
    Checking if the claw is triggered then will emit a red light 5 times.

--- Levels/Engine/Scripts/dock.lua
+++ Levels/Engine/Scripts/dock.lua
@@ -2,7 +2,7 @@ local Util = require("Util")
 Util.ShortenTENCalls()
 
 LevelFuncs.OnControlPhase = function()
-    ShivaBossJoints = -- looking at ShivaBoss face on . S=ShivaBoss
+--[[    ShivaBossJoints = -- looking at ShivaBoss face on . S=ShivaBoss
        {
        SPelvis = 0,
            SR_Thigh = 1,
@@ -63,6 +63,7 @@ LevelFuncs.OnControlPhase = function()
                       SSLB_Blade = 25,
            SSHead=2,
    }
+]]
 --lara--
 local lara = GetMoveableByName("lara")
 local laraPos = lara:GetPosition()
@@ -90,20 +91,18 @@ function ElevatorDefinitions()--Doors--
     if 
     laraRoom == LiftGF and laraPos
         then
-            --LiftDoor_0_L:SetStatus(0)
-            --LiftDoor_1_L:SetStatus(0)
-            --LiftDoor_0_R:SetStatus(0)
-            --LiftDoor_1_R:SetStatus(0)
+            LiftDoor_0_L:SetStatus(0)
+            LiftDoor_1_L:SetStatus(0)
+            LiftDoor_0_R:SetStatus(0)
+            LiftDoor_1_R:SetStatus(0)
             lara.y = laraPos.y
             lara:GetPosition()
             lara:SetPosition(lara.y + 3072)
             lara:GetRoom()
             lara:SetRoom(53)
-            
-            --[[
             Table1: SetRoom (Lift1F)
             Table1.y= Table1Pos.y
-            Table1:SetPosition(Table1.y + 3072)]]
+            Table1:SetPosition(Table1.y + 3072)
     end
 end
 --[[=================Shiva Boss Start========================================
@@ -208,32 +207,78 @@ LevelFuncs.Shiva = function()
     end--]]
 function InitClaw2local()
     
-    local Claw=GetMoveableByName("wrecking_ball")
-
+    local Claw=GetMoveableByName("claw")
+        local ClawOn=Claw:GetStatus()
+        local ClawPos = Claw:GetPosition()
     local ClawTop = GetMoveableByName("claw_top")
-
+        local ClawTopOn=ClawTop:GetStatus()
+        local ClawTopPos = ClawTop:GetPosition()
     local ClawChain = GetMoveableByName("claw_chain")
-
-    local ClawFloorPos = GetMoveableByName("claw_target")
-
-
-    
+        local ClawChainOn=ClawChain:GetStatus()
+        local ClawChainPos = ClawChain:GetPosition()
+    local ClawFloorTarget = GetMoveableByName("ClawFloorTarget")
+        local ClawFloorTargetPos = ClawFloorTarget:GetPosition()
+    --lighting
+        local clawoncolor = Color(255,0,0)
+        if ClawOn == 1 
+        then 
+        Effects.EmitLight(ClawPos,clawoncolor,1000)
 end
-
----=================Shiva Boss End=====================
-
---==================Elevator==================
-
+end
 LevelFuncs.InitClaw = function()
-
+    InitClaw2local()
     end
 
 
 
 LevelFuncs.ClawMovement = function()
-    InitClaw2local()
+    --InitClaw2local()
 end
 
 LevelFuncs.ClawStop = function()
+---=================Shiva Boss End=====================
+
+--==================Elevator==================
+
+    LevelFuncs.Lift = function()
+        local lara = GetMoveableByName("lara")
+        local laraPos = lara:GetPosition()
+        local laraRoom = lara:GetRoom()
+        function ElevatorDefinitions()--Doors--
+            --Ground Floor
+                local LiftDoor_0_L = GetMoveableByName("LiftDoor_0_L")
+                    local LiftDoor_0_L_Check = LiftDoor_0_L: GetStatus()
+                local LiftDoor_0_R = GetMoveableByName("LiftDoor_0_R")
+                    local LiftDoor_0_R_Check = LiftDoor_0_R: GetStatus()
+            --1st Floor
+                local LiftDoor_1_L = GetMoveableByName("LiftDoor_1_L")
+                    local LiftDoor_1_LCheck = LiftDoor_1_L: GetStatus()
+                local LiftDoor_1_R = GetMoveableByName("LiftDoor_1_R")
+                    local LiftDoor_1_RCheck = LiftDoor_1_R: GetStatus()
+            --Pushable Table --
+            local Table1 = GetMoveableByName("Table1")
+            local Table1Pos = Table1:GetPosition()
+            local Table1Room = Table1:GetRoom()
+            --Lift--
+            local LiftGF = 51
+            local Lift1F = 53
+            --Logic--
+
+                    LiftDoor_0_L:SetStatus(0)
+                    LiftDoor_1_L:SetStatus(0)
+                    LiftDoor_0_R:SetStatus(0)
+                    LiftDoor_1_R:SetStatus(0)
+                    lara.y = laraPos.y
+                    lara:GetPosition()
+                    lara:SetPosition(lara.y + 3072)
+                    lara:GetRoom()
+                    lara:SetRoom(53)
+                    Table1: SetRoom (Lift1F)
+                    Table1.y= Table1Pos.y
+                    Table1:SetPosition(Table1.y + 3072)
+
+
+end
+end
 end
 end
